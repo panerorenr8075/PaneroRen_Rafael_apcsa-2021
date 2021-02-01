546ef333db96097814bdec2eedb7e8e64e226d96
panerorenr8075 <78329074+panerorenr8075@users.noreply.github.com>
2021-01-31 20:41:15 -0800

    ASCII Box

---
+++ project0/src/project0/box.java
@@ -0,0 +1,22 @@
+package project0;
+
+public class box {
+
+	public static void main(String[] args) {
+		System.out.println("Rafael Panero Ren \t  January 31, 2021 \n\n" );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+
+	}
+
+}
