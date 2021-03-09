d848eaa19472e9fea9f7f32754055bdfadd0fdae
panerorenr8075 <78329074+panerorenr8075@users.noreply.github.com>
2021-03-09 06:32:45 -0800

    Add files via upload

---
+++ ElevensLab/src/elevenslab/CardTester.java
@@ -0,0 +1,19 @@
+package elevenslab;
+
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		
+		Card test = new Card("B", "Spade", 2);
+		
+		System.out.println(test);
+	}
+}
